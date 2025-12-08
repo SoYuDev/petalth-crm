ac44d160480c50d7e4b88ee55e63585a790e880e
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2025-12-08 19:25:24 +0100

    Initial commit

---
+++ pealth-backend/src/main/java/com/luis/pealthbackend/PealthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.luis.pealthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PealthBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PealthBackendApplication.class, args);
+    }
+
+}
