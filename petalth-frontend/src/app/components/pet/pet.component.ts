e9f11940cb07daf9414e14a06ce92306f60c4fb9
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2026-01-23 13:42:55 +0100

    feat: update pet component

--- petalth-frontend/src/app/components/pet/pet.component.ts
+++ petalth-frontend/src/app/components/pet/pet.component.ts
@@ -3,31 +3,54 @@ import { PetService } from './pet.service';
 import { Pet } from './pet';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../security/service/auth.service';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pet',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './pet.component.html',
   styleUrl: './pet.component.css',
 })
 export class PetComponent implements OnInit {
+  // Inyección dependencias.
   private petService = inject(PetService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private authService = inject(AuthService);
+  private fb = inject(FormBuilder);
 
   pets = signal<Pet[]>([]);
+
+  // Variable para mostrar el formulario de mascotas
+  showForm = signal<boolean>(false);
+
+  currentOwnerId!: number;
+
+  // Formulario para la mascota
+  petForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    birthDate: ['', [Validators.required]],
+    photoUrl: [''], // Opcional
+  });
 
   ngOnInit(): void {
     // Escuchamos la URL
     this.route.paramMap.subscribe((params) => {
+      // Debe de tener obligatoriamente el nombre de la ruta de app.routes.ts
       const idString = params.get('ownerId');
 
       if (idString) {
         // CASO A: Estoy en /pets/5 -> Cargo los datos
-        const ownerId = Number(idString);
-        this.loadPets(ownerId);
+        // Casi siempre vamos a tener el Caso A ya que desde el navbar se construye esa URL, el caso b protege si ponemos /pets en el navegador.
+        this.currentOwnerId = Number(idString);
+        this.loadPets(this.currentOwnerId);
       } else {
         // CASO B: Estoy en /pets (sin nada) -> Redirijo
         const myId = this.authService.currentUser()?.id;
@@ -46,4 +69,6 @@ export class PetComponent implements OnInit {
       this.pets.set(data);
     });
   }
+
+  /* TODO - CRUD */
 }
