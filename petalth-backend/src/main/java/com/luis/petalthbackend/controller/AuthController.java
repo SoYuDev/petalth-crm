14dbce3a307563b1b27690ee3eeccc9fd95194e6
Luis Rodríguez <luisrodriguezcamas99@gmail.com>
2026-04-06 17:27:29 +0200

    fix: fix swagger doc

--- petalth-backend/src/main/java/com/luis/petalthbackend/controller/AuthController.java
+++ petalth-backend/src/main/java/com/luis/petalthbackend/controller/AuthController.java
@@ -16,13 +16,15 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @Operation(summary = "Iniciar sesión", description = "Verifica las credenciales y devuelve el token JWT")
+    @Operation(summary = "Iniciar sesión", description = "Verifica las credenciales y devuelve el DTO" +
+            " AuthResponse que contiene el token JWT")
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok(authService.login(loginRequest));
     }
 
-    @Operation(summary = "Registrar usuario", description = "Crea una cuenta nueva y devuelve el token JWT")
+    @Operation(summary = "Registrar usuario", description = "Crea una cuenta nueva y devuelve el DTO" +
+            " AuthResponse que contiene el token JWT")
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
         return ResponseEntity.ok(authService.register(registerRequest));
